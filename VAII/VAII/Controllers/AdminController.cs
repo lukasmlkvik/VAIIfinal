0a1366da91af2229b4d6a772db6557908374db7e
lukasmlkvik <65865116+lukasmlkvik@users.noreply.github.com>
2021-01-16 21:30:27 +0100

    admin

--- VAII/VAII/Controllers/AdminController.cs
+++ VAII/VAII/Controllers/AdminController.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using VAII.Data;
 
 namespace VAII.Controllers
 {
     public class AdminController : Controller
     {
+
+        private ApplicationDbContext _db;
+
+        public AdminController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<string> l = new List<string>();
+            if ("admin@admin.admin".Equals(User.Identity.Name))
+            {
+                l = _db.Users.Select(u=>u.Email).ToList();
+            }
+            return View(l);
         }
     }
 }
